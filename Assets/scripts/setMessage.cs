1a9e94a55037988265e6d34c04746ef17a9bf028
wxz97121 <wxz97121@gmail.com>
2017-03-30 00:52:30 +0800

    Add Notebook by BottleFish.

---
+++ Assets/scripts/setMessage.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class setMessage : MonoBehaviour {
+    public Button backButton;
+    public GameObject noteBook;
+    public GameObject shadow;
+    public CanvasGroup noteBookAlph;
+    public CanvasGroup shadowAlph;
+    public Text text;
+    public Image image;
+
+    public void Start () {
+        noteBookAlph.alpha = 0;
+        shadowAlph.alpha = 0;
+        noteBook.SetActive(false);
+        shadow.SetActive(false);
+        backButton.transform.SetSiblingIndex(100);                               
+	}
+    IEnumerator disappear()
+    {
+        yield return new WaitForSeconds(1.0f);
+        noteBook.SetActive(false);
+        shadow.SetActive(false);
+    }	
+
+	void Update () {		
+	}
+    public void showBook()
+    {
+        GetConcept();
+        noteBook.SetActive(true);
+        noteBookAlph.DOFade(1, 1);
+        shadow.SetActive(true);
+        shadowAlph.DOFade(0.7f, 1);
+    }
+    public void EscN()
+    {
+        noteBookAlph.DOFade(0, 1);
+        shadowAlph.DOFade(0, 1);
+        StartCoroutine(disappear());      
+    }
+    void GetConcept()
+    {
+
+    }
+}
